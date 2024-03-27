c897ecbec3f5412761e574b6f5e5dab85bd58d39
boostedd2 <45469010+boostedd2@users.noreply.github.com>
2024-03-26 21:39:29 -0400

    thank you message

--- src/components/common/NavBlocks.jsx
+++ src/components/common/NavBlocks.jsx
@@ -26,12 +26,16 @@ export default function NavBlocks() {
     message: "",
   });
 
+  const [formSending, setFormSending] = React.useState(false);
+  const [formSent, setFormSent] = React.useState(false);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = async (event) => {
+    setFormSending(true)
     event.preventDefault();
 
     try {
@@ -46,6 +50,9 @@ export default function NavBlocks() {
       if (!response.ok) {
         throw new Error("Failed to submit form data");
       }
+
+      setFormSending(false)
+      setFormSent(true)
 
       setFormData({
         name: "",
@@ -112,53 +119,72 @@ export default function NavBlocks() {
             </DialogClose>
           </div>
 
-          <div class="w-full flex items-center">
-            <form onSubmit={handleSubmit} class="w-full">
-              <input type="text" name="firstName" value={formData.firstName} onChange={handleInputChange} style={{ display: 'none' }} />
-              <input type="text" name="lastName" value={formData.lastName} onChange={handleInputChange} style={{ display: 'none' }} />
-
-              <div class="mb-4 pt-4">
-                <Input
-                  class="h-[50px] appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          {formSending || formSent ? null : (
+            <div class="w-full flex items-center">
+              <form onSubmit={handleSubmit} class="w-full">
+                <input
                   type="text"
-                  placeholder="Name"
-                  required
-                  name="name"
-                  value={formData.name}
+                  name="firstName"
+                  value={formData.firstName}
                   onChange={handleInputChange}
+                  style={{ display: "none" }}
                 />
-              </div>
-              <div class="mb-4">
-                <Input
-                  class="h-[50px] appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  type="email"
-                  placeholder="Email Address"
-                  required
-                  name="email"
-                  value={formData.email}
+                <input
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
                   onChange={handleInputChange}
+                  style={{ display: "none" }}
                 />
-              </div>
-              <div class="mb-6">
-                <textarea
-                  class="h-[200px] appearance-none resize-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  placeholder="Message"
-                  required
-                  name="message"
-                  value={formData.message}
-                  onChange={handleInputChange}
-                ></textarea>
-              </div>
-              <div class="flex items-center justify-center">
-                <button
-                  class="flex justify-center items-center w-full max-w-[100px] h-[45px] bg-white hover:bg-teal-400 text-teal-400 hover:text-white hover-text-effect-plain border border-teal-400 font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
-                  type="submit"
-                >
-                  <IconSend2 />
-                </button>
-              </div>
-            </form>
-          </div>
+
+                <div class="mb-4 pt-4">
+                  <Input
+                    class="h-[50px] appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    type="text"
+                    placeholder="Name"
+                    required
+                    name="name"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div class="mb-4">
+                  <Input
+                    class="h-[50px] appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    type="email"
+                    placeholder="Email Address"
+                    required
+                    name="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div class="mb-6">
+                  <textarea
+                    class="h-[200px] appearance-none resize-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    placeholder="Message"
+                    required
+                    name="message"
+                    value={formData.message}
+                    onChange={handleInputChange}
+                  ></textarea>
+                </div>
+                <div class="flex items-center justify-center">
+                  <button
+                    class="flex justify-center items-center w-full max-w-[100px] h-[45px] bg-white hover:bg-teal-400 text-teal-400 hover:text-white hover-text-effect-plain border border-teal-400 font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
+                    type="submit"
+                  >
+                    <IconSend2 />
+                  </button>
+                </div>
+              </form>
+            </div>
+          )}
+
+          {formSent && 
+          <div>
+            <p className="text-3xl text-teal-400 text-center">Message sent, speak to you soon!</p>
+            </div>}
         </DialogContent>
       </Dialog>
     </div>
